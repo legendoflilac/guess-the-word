7fa905a47a61416b0c2e5b5ca5d6ee12ee0ead55
Ranelle_I <43350178+legendoflilac@users.noreply.github.com>
2023-10-17 08:51:43 -0500

    Added input validation and guessing logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersList = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function(word) {
     let array = word.split("");
@@ -22,7 +23,38 @@ placeholder(word);
 
 guessButton.addEventListener("click", function(e) {
     e.preventDefault();
+    message.innerText = "";
     const input = letterInput.value;
-    console.log(input);
+    const result = validateInput(input);
+    if (result) {
+        makeGuess(result);
+    }
     letterInput.value = "";
 });
+
+const validateInput = function(input) {
+    const acceptedLetter = /[a-zA-Z]/
+    if (input === "") {
+        message.innerText = "Please enter a letter from A-Z."
+    }
+    else if (input.length > 1) {
+        message.innerText = "Please enter one letter."
+    }
+    else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A-Z."
+    }
+    else {
+        return input;
+    }
+}
+
+const makeGuess = function(letter) {
+    const upperLetter = letter.toUpperCase();
+    if (guessedLetters.includes(upperLetter)) {
+        message.innerText = "You already guessed this letter. Try again."
+    }
+    else {
+        guessedLetters.push(upperLetter);
+    }
+    console.log(guessedLetters);
+}
